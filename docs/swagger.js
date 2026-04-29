c8b831364c3122a0e5c804a1956eacc2ad1de319
Martin Maximiliano Perez <pericoargento@gmail.com>
2026-04-29 08:09:28 -0300

    Respaldo de mi parte antes de actualizar todo

---
+++ docs/swagger.js
@@ -0,0 +1,28 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API TFI Programación III - Grupo M',
+      version: '1.0.0',
+      description: 'Documentación de la API para el Trabajo Final Integrador. Incluye la gestión de especialidades, usuarios y turnos.',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Servidor Local',
+      },
+    ],
+  },
+  // Aquí le decimos a Swagger dónde buscar los comentarios para documentar
+  apis: ['./routes/*.js'], 
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export const setupSwagger = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  console.log('✅ Documentación disponible en http://localhost:3000/api-docs');
+};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { setupSwagger } from './docs/swagger.js';
 
 process.loadEnvFile();
 
@@ -8,6 +9,10 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// --- ESTO ES LO QUE FALTA ---
+setupSwagger(app); 
+// ----------------------------
 
 const PUERTO = process.env.PUERTO || 3000;
 app.listen(PUERTO, () => {
